9b3d20c4841082ec1139edc99ff4349474fc4dcf
muhalfian81 <105841118122@student.unismuh.ac.id>
2026-02-19 13:45:26 +0800

    Kirim codingan asli

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+// Pastikan bagian ini ada dan di-export!
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
